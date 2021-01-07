8b562a393a319ee8d821d0f4b368e3bc27aa0a39
Daniel Weiland <49922377+dannyweiland@users.noreply.github.com>
2021-01-07 12:34:14 -0800

    project

--- TreeMap.r
+++ TreeMap.r
@@ -1,8 +1,13 @@
+#This is the code for the treemap visualization.  It relies on df1 from the main CarMax.rmd file 
+
+#Import treemap library
 library(treemap)
 
+#Make a dataframe with the count of the purchase make and purchase model
 treemap_df <- df1 %>%
   count(purchase_make, purchase_model)
 
+#Create the Treemap showing the makeup of the makes and models in the dataset
 MakeModelTreemap <- treemap(treemap_df,
                             index=c("purchase_make", "purchase_model"),
                             vSize="n",
--- wordCloud.r
+++ wordCloud.r
@@ -8,6 +8,9 @@ library(ggpubr)
 #Import Dataset
 testData <- read.csv("CaseCompetitionData2021Copy.csv")
 
+#Drop post-purchase because of excessive NA
+testData <- select(testData, -post_purchase_satisfaction)
+
 #Remove NA
 testData[testData=="?"]<- NA
 testData <- na.omit(testData)
@@ -17,22 +20,3 @@ cd1 <- count(testData, purchase_make)
 
 #Create Wordcloud
 wordcloud(words=cd1$purchase_make, freq = cd1$n, min.freq = 3)
-
-#Baloon Plot -----
-
-#Rearrange data as factor data
-testData <- testData %>%
-  mutate(customer_income = fct_relevel(customer_income, c("0 - 20000", "20001 - 40000", "40001 - 60000", "60001 - 80000", "80001 - 100000", "100001 - 120000", "120001 - 140000", "140001 - 160000", "160001 - 180000", "180001 - 200000",  "200001+"))) %>%
-  mutate(customer_income = fct_rev(customer_income)) %>%
-  arrange(customer_income) %>%
-  mutate(customer_age = fct_relevel(customer_age, c("0 - 20", "21 - 30", "31 - 40", "41 - 50", "51 - 60", "61 - 70", "71 - 80", "81 - 90", "101+"))) %>%
-  arrange(customer_age)
-
-#Create contingency table
-contingencyTable <- table(testData$customer_age, testData$customer_income)
-
-contingencyTable <- as.data.frame(contingencyTable)
-
-ggballoonplot(contingencyTable, fill = "value")+
-  scale_fill_viridis_c(option = "C")
-
